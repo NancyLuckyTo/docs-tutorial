ef7bef92532ac6c24fd66575934d70c189af1b95
NancyLuckyTo <NancyLuckyTo@icloud.com>
2025-06-25 09:42:03 +0800

    feat: Create (home)navbar.tsx, page.tsx

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -87,7 +87,7 @@ export const Navbar = () => {
     <nav className="flex items-center justify-between">
       <div className="flex gap-2 items-center">
         <Link href="/">
-          <Image src="/logo.svg" alt="Logo" width={36} height={36} />
+          <Image src="/logo.svg" alt="Logo" width={32} height={32} />
         </Link>
         <div className="flex flex-col">
           <DocumentInput />
